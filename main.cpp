80910c261b72f4fddb475c4d2838e803860beea9
洋輝 大橋 <hiro0084169@outlook.jp>
2023-10-02 11:28:05 +0900

    01_01

--- main.cpp
+++ main.cpp
@@ -1,5 +1,43 @@
 #include <stdio.h>
 
+template <typename Type>
+Type Min(Type a, Type b) {
+
+	if(a > b) {
+		return a;
+	}
+	else if (a < b) {
+		return b;
+	}
+}
+
+// char型の場合
+char Min(char a[], char b[]) {
+	printf("数字以外は代入できません");
+	return 0;
+}
+
 int main() {
+
+	int a = 10;
+	int b = 20;
+	
+	printf("%d\n", Min(a, b));
+
+	float c = 10.0f;
+	float d = 54.0f;
+
+	printf("%f\n", Min(c, d));
+
+	double e = 30;
+	double f = 20;
+
+	printf("%f\n", Min(e, f));
+
+	char g[] = "aaaa";
+	char h[] = "bbbb";
+
+	printf("%c\n", Min(g, h));
+
 	return 0;
 }
