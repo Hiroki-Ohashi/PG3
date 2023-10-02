598e2cb5cd2108734a7b8f79dc4c1778260df7b1
洋輝 大橋 <hiro0084169@outlook.jp>
2023-10-02 12:45:05 +0900

    01_02

--- main.cpp
+++ main.cpp
@@ -1,21 +1,25 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 
-int Recursive(int n) {
-	if (n <= 100) {
-		return 100;
-	}
+int Recursive(int num1, int num2, int num3,int count) {
+	// 働いた時間
+	count += 1;
+	// 一般的な賃金体系
+	num1 = num1 * count;
+	// 再帰的な賃金体系
+	num2 = num2 * 2 - 50;
+	printf("%d時間 一般的:%d 再帰的:%d\n", count, num1, num2);
 
-	return (Recursive(n) * 2 - 50);
+	return Recursive(num1, num2, num3, count);
 }
 
 int main() {
 
-	int n = 100;
-	int result;
+	int num1 = 100;
+	int num2 = 1072; 
+	int num3 = 0;
+	int count = 1;
 
-	result = Recursive(n);
-	printf("%dの階乗 = %d\n", n, result);
-
+	Recursive(num1, num2, num3, count);
 
 	return 0;
 }
