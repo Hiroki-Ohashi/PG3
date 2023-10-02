f423a6e8c8d78c2ee5930d5ec09e7d35d2ddb377
洋輝 大橋 <hiro0084169@outlook.jp>
2023-10-02 11:49:30 +0900

    01_02

--- main.cpp
+++ main.cpp
@@ -1,42 +1,21 @@
 ﻿#include <stdio.h>
 
-template <typename Type>
-Type Min(Type a, Type b) {
-	if(a > b) {
-		return a;
+int Recursive(int n) {
+	if (n <= 100) {
+		return 100;
 	}
-	else {
-		return b;
-	}
-}
 
-// char型の場合
-char Min(char a[], char b[]) {
-	printf("数字以外は代入できません");
-	return 0;
+	return (Recursive(n) * 2 - 50);
 }
 
 int main() {
 
-	int a = 10;
-	int b = 20;
-	
-	printf("%d\n", Min(a, b));
+	int n = 100;
+	int result;
 
-	float c = 10.0f;
-	float d = 54.0f;
+	result = Recursive(n);
+	printf("%dの階乗 = %d\n", n, result);
 
-	printf("%f\n", Min(c, d));
-
-	double e = 30;
-	double f = 20;
-
-	printf("%lf\n", Min(e, f));
-
-	char g[] = "aaaa";
-	char h[] = "bbb";
-
-	printf("%c\n", Min(g, h));
 
 	return 0;
 }
