030bf6768679ec7ff7973d31d166bd4fe71b1336
洋輝 大橋 <hiro0084169@outlook.jp>
2023-10-16 12:17:28 +0900

    01_02

--- main.cpp
+++ main.cpp
@@ -1,25 +1,41 @@
 #include <stdio.h>
 
-int Recursive(int num1, int num2, int num3,int count) {
-	// 働いた時間
-	count += 1;
-	// 一般的な賃金体系
-	num1 = num1 * count;
-	// 再帰的な賃金体系
-	num2 = num2 * 2 - 50;
-	printf("%d時間 一般的:%d 再帰的:%d\n", count, num1, num2);
+int Recursive(int num1, int num2, int num3, int num4, int count) {
+	// 時給
+	printf("%d時間の再帰的時給: %d\n", count, num2);
 
-	return Recursive(num1, num2, num3, count);
+	// 再帰的
+	num1 += num2;
+
+	// 給料
+	printf("%d時間の再帰的給料: %d\n", count, num1);
+	num2 = num2 * 2 - 50;
+
+	// 一般的
+	num3 += num4;
+
+	printf("%d時間の一般的給料: %d\n", count, num3);
+	printf("\n");
+
+	if (num2 >= num3) {
+		return num2;
+	}
+
+	// 時間
+	count++;
+
+	return (num1 + Recursive(num1, num2, num3, num4, count));
 }
 
 int main() {
 
-	int num1 = 100;
-	int num2 = 1072; 
+	int num1 = 0;
+	int num2 = 100;
 	int num3 = 0;
+	int num4 = 1072;
 	int count = 1;
 
-	Recursive(num1, num2, num3, count);
+	Recursive(num1, num2, num3, num4, count);
 
 	return 0;
 }
