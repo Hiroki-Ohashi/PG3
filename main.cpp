2d5b5d4c76c241edfc93aee20da5bdc31237e566
洋輝 大橋 <hiro0084169@outlook.jp>
2023-11-06 12:47:01 +0900

    01_03

--- main.cpp
+++ main.cpp
@@ -1,27 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <windows.h>
 
-int even(int num) {
-	2 * num;
+typedef int (*PFunc)();
+
+void Answer(PFunc p, int answer) {
+	int dice = p() % 2;
+	int anticipation = answer % 2;
+	Sleep(3000);
+
+	printf("回答:");
+
+	// 偶数なら丁
+	if (dice == 0) {
+		printf("丁\n");
+		Sleep(1500);
+	}// 奇数なら半
+	else if (dice != 0) {
+		printf("半\n");
+		Sleep(1500);
+	}
+
+	// どちらも同じ数なら正解
+	if (anticipation == dice) {
+		printf("正\n\n");
+	}
+	else {
+		printf("誤\n\n");
+	}
 }
 
-int odd(int num) {
-	2 * num + 1;
-}
-
-int dice(int num) {
-	int currentTime = time(nullptr);
-	srand(currentTime);
-	num = rand()% 6 + 1;
-	return printf("%d", num);
+int dice() {
+	int result = rand() % 6 + 1;
+	return result;
 }
 
 int main() {
+	// サイコロを決定
+	int currentTime = time(nullptr);
+	srand(currentTime);
 
-	int num = 0;
+	// 関数ポインタ
+	PFunc pfunc = &dice;
+	// 入力した予想
+	int answer;
 
-	dice(num);
+	printf("丁( 2 )、半( 1 )選べ\n\n");
+	while (1) {
+		printf("丁( 2 )、半( 1 )を入力\n");
+		printf("予想:");
+		scanf_s("%d", &answer);
+
+		Answer(pfunc, answer);
+	}
 
 	return 0;
 }
