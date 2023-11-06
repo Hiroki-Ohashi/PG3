cb17cd187829d5bd541db47cb9e3ca542824102a
洋輝 大橋 <hiro0084169@outlook.jp>
2023-11-06 14:28:44 +0900

    01_04

--- main.cpp
+++ main.cpp
@@ -2,48 +2,45 @@
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h>
-
-typedef int (*PFunc)();
-
-void Answer(PFunc p, int answer) {
-	int dice = p() % 2;
-	int anticipation = answer % 2;
-	Sleep(3000);
-
-	printf("回答:");
-
-	// 偶数なら丁
-	if (dice == 0) {
-		printf("丁\n");
-		Sleep(1500);
-	}// 奇数なら半
-	else if (dice != 0) {
-		printf("半\n");
-		Sleep(1500);
-	}
-
-	// どちらも同じ数なら正解
-	if (anticipation == dice) {
-		printf("正\n\n");
-	}
-	else {
-		printf("誤\n\n");
-	}
-}
-
-int dice() {
-	int result = rand() % 6 + 1;
-	return result;
-}
+#include <functional>
 
 int main() {
+
+	std::function<void(int, int)> result = [&](int p, int answer) {
+		int dice = p % 2;
+		int anticipation = answer % 2;
+		Sleep(3000);
+
+		printf("回答:");
+
+		// 偶数なら丁
+		if (dice == 0) {
+			printf("丁\n");
+		}// 奇数なら半
+		else if (dice != 0) {
+			printf("半\n");
+		}
+
+		// どちらも同じ数なら正解
+		if (anticipation == dice) {
+			printf("結果:正\n\n");
+		}
+		else {
+			printf("結果:誤\n\n");
+		}
+	};
+
 	// サイコロを決定
 	int currentTime = time(nullptr);
 	srand(currentTime);
 
-	// 関数ポインタ
-	PFunc pfunc = &dice;
-	// 入力した予想
+	std::function<int()> dice = [&]() {
+		int result = rand() % 6 + 1;
+		return result;
+	};
+
+	
+	// 予想
 	int answer;
 
 	printf("丁( 2 )、半( 1 )選べ\n\n");
@@ -52,7 +49,7 @@ int main() {
 		printf("予想:");
 		scanf_s("%d", &answer);
 
-		Answer(pfunc, answer);
+		result(dice(), answer);
 	}
 
 	return 0;
