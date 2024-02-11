bd991b188adb80610983775c697e0631c56dbea3
洋輝 大橋 <hiro0084169@outlook.jp>
2024-02-12 00:15:31 +0900

    01_03

--- main.cpp
+++ main.cpp
@@ -2,55 +2,60 @@
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h>
+using PFunc = void (*)(int);
 
-typedef int (*PFunc)();
-
-void Answer(PFunc p, int answer) {
-	int dice = p() % 2;
-	int anticipation = answer % 2;
-	Sleep(3000);
-
-	printf("回答:");
-
-	// 偶数なら丁
-	if (dice == 0) {
-		printf("丁\n");
-	}// 奇数なら半
-	else if (dice != 0) {
-		printf("半\n");
-	}
-
-	// どちらも同じ数なら正解
-	if (anticipation == dice) {
-		printf("正\n\n");
-	}
-	else {
-		printf("誤\n\n");
-	}
-}
-
-int dice() {
+// サイコロの目
+int Dice() {
 	int result = rand() % 6 + 1;
 	return result;
 }
 
-int main() {
-	// サイコロを決定
-	int currentTime = time(nullptr);
-	srand(currentTime);
+// 答え
+void Answer(int answer) {
+	int dice = Dice();
+	// 偶数なら丁
+	if (dice % 2 == 0) {
+		printf("丁\n");
+		Sleep(1000 * 1.5f);
+	}// 奇数なら半
+	else if (dice % 2 != 0) {
+		printf("半\n");
+		Sleep(1000 * 1.5f);
+	}
 
+	// 同じなら正解
+	if (answer == 1 && dice % 2 != 0) {
+		printf("正解\n\n");
+	}
+	else if (answer == 2 && dice % 2 == 0) {
+		printf("正解\n\n");
+	}
+	else {
+		printf("不正解\n\n");
+	}
+}
+
+void SetTimeout(PFunc p, int time, int answer) {
+	printf("答え\n");
+	Sleep(1000 * time);
+	p(answer);
+}
+
+int main() {
+	// サイコロの目を決定する
+	srand((unsigned int)time(nullptr));
 	// 関数ポインタ
-	PFunc pfunc = &dice;
-	// 入力した予想
+	PFunc pfunc = Answer;
+	// 入力予想
 	int answer;
 
-	printf("丁( 2 )、半( 1 )選べ\n\n");
+	printf("サイコロは丁( 2 )、半( 1 )\n\n");
 	while (1) {
-		printf("丁( 2 )、半( 1 )を入力\n");
+		printf("丁( 2 )、半( 1 )どちらかを入力\n");
 		printf("予想:");
 		scanf_s("%d", &answer);
 
-		Answer(pfunc, answer);
+		SetTimeout(pfunc, 3, answer);
 	}
 
 	return 0;
